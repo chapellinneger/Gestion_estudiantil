94f83ec61e110552992e601ad7ea949ff32de183
Vin <hlh18go@gmail.com>
2026-02-04 14:36:03 -0400

    para mandar las tareas V

--- addons/gestion_profesorado/__manifest__.py
+++ addons/gestion_profesorado/__manifest__.py
@@ -13,6 +13,7 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/activity_views.xml',
     ],
     'demo': [],
     'installable': True,
