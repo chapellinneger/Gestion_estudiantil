9861d6bbf780626f4f3b033de66a296d77f4d9cf
jessiesb <jessiesb19@gmail.com>
2026-02-03 22:03:51 -0400

    Add 'gestion_materia' dependency and category

--- addons/gestion_secciones/__manifest__.py
+++ addons/gestion_secciones/__manifest__.py
@@ -4,7 +4,8 @@
     "description": "Este modulo permite gestionar las secciones en un sistema de gestion estudiantil.",
     "version": "18.0.1.0.0",
     "author": "Kaazest",
-    "depends": ["base"],
+    "category": "Education",
+    "depends": ["base", "gestion_materia"],
     "data": [
         "security/ir.model.access.csv",
         "views/gestion_seccion_views.xml",
