f7c10dc01db0d217af873a41dc82a40fd6cc75dc
jessiesb <jessiesb19@gmail.com>
2026-02-05 15:54:00 -0400

    Add domain filter for teacher_id and fix state string

--- addons/gestion_secciones/models/gestion_seccion.py
+++ addons/gestion_secciones/models/gestion_seccion.py
@@ -6,12 +6,12 @@ class GestionSeccion(models.Model):
 
     name = fields.Char(required=True)
     subject_id = fields.Many2one("gestion.materia", string="Materia", required=True)
-    teacher_id = fields.Many2one("res.partner", string="Profesor")
+    teacher_id = fields.Many2one("res.partner", string="Profesor", domain=[("is_teacher", "=", True)])
     student_ids = fields.Many2many("res.partner", string="Estudiantes")
     state = fields.Selection(
         [
             ("abierto", "Abierto"),
-            ("en_progreso", "En Progreso"),
+            ("en progreso", "En Progreso"),
             ("finalizado", "Finalizado"),
         ],
         default="abierto",
